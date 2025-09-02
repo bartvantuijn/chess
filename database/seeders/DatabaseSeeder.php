34a0e61c2291f1df301a6eb80fde975fa82bf8f6
Bart van Tuijn <bart@bartvantuijn.nl>
2025-09-02 11:47:06 +0000

    Fix code style

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,7 +7,6 @@ use App\Models\Opening;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
